80928daaf41c67663f24fa61c60c4da1df284f91
Iyiola Akanbi <iyiola3332@gmail.com>
2021-04-05 18:46:19 +0100

    apod service and view

--- lib/networking/requestService.dart
+++ lib/networking/requestService.dart
@@ -6,10 +6,15 @@ import 'package:http/http.dart' as http;
 
 class ApodService {
   Future<List<Apod>> getApod(String startDate, String endDate) async {
+    List<Apod> apod;
     try {
       var res = await http.get(
           '$baseUrl?api_key=$apiKey&start_date=$startDate&end_date=$endDate');
-      if (res.statusCode == 200) return jsonDecode(res.body);
+      if (res.statusCode == 200) {
+        List result = jsonDecode(res.body) as List;
+        apod = result.map((e) => Apod.fromJson(e)).toList();
+      }
+      return apod;
     } catch (e) {
       print(e.toString());
       throw (e);
---
+++ lib/screens/apodView.dart
@@ -0,0 +1,16 @@
+import 'package:APOD/models/apodModel.dart';
+import 'package:flutter/material.dart';
+
+class ApodView extends StatefulWidget {
+  List<Apod> apod = List();
+  ApodView({this.apod});
+  @override
+  _ApodViewState createState() => _ApodViewState();
+}
+
+class _ApodViewState extends State<ApodView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
