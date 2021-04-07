ff0546d0563696adc8bf3a36f5ddcf7e4ade6b99
Iyiola Akanbi <iyiola3332@gmail.com>
2021-04-07 02:43:07 +0100

    view model

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 50,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.grey),
+                  ),
+                ),
+                Text(
+                  '\n-',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  height: 50,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.grey),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/vm/homeVm.dart
@@ -0,0 +1,71 @@
+import 'dart:html';
+
+import 'package:APOD/models/apodModel.dart';
+import 'package:APOD/networking/apiRequestStatus.dart';
+import 'package:APOD/networking/requestService.dart';
+import 'package:APOD/screens/apodView.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class ApodProvider extends ChangeNotifier {
+  ApiRequestStatus status = ApiRequestStatus.uninitialized;
+  DateTime startDate = DateTime.now();
+  DateTime endDate = DateTime.now();
+  DateFormat formatter = new DateFormat('yyyy-MM-dd');
+  ApodService service = ApodService();
+
+  Future<void> getApod(BuildContext context) async {
+    String formattedStartDate = formatter.format(startDate);
+    String formattedEndDate = formatter.format(endDate);
+    try {
+      status = ApiRequestStatus.loading;
+      notifyListeners();
+      List<Apod> model =
+          await service.getApod(formattedStartDate, formattedEndDate);
+      status = ApiRequestStatus.loaded;
+      notifyListeners();
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ApodView(
+                    apod: model,
+                  )));
+    } catch (e) {
+      status = ApiRequestStatus.error;
+      notifyListeners();
+    }
+  }
+
+  Future<void> getStartDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: startDate,
+        firstDate: DateTime(1995, 6, 16),
+        lastDate: startDate);
+    if (picked != null && picked != startDate) {
+      startDate = picked;
+      notifyListeners();
+    } else {
+      SnackBar(
+          content:
+              Text("picked date can neither empty nor same with today's date"));
+    }
+  }
+
+  Future<void> getEndDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: endDate,
+        firstDate: DateTime(1995, 6, 16),
+        lastDate: endDate);
+    if (picked != null && picked != endDate) {
+      endDate = picked;
+      notifyListeners();
+    } else {
+      SnackBar(
+          content:
+              Text("picked date can neither empty nor same with today's date"));
+    }
+  }
+}
